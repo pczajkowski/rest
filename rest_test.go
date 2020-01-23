5140f5da2e491ff89bff6b8430f094537dd61583
Piotr Czajkowski <pczajkowski@gmail.com>
2020-01-23 17:38:11 +0100

    Added TestGET

--- rest_test.go
+++ rest_test.go
@@ -1,7 +1,10 @@
 package rest
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -46,5 +49,38 @@ func TestJSONDecoderBadJSON(t *testing.T) {
 
 	if expected.First == result.First || expected.Second == result.Second {
 		t.Errorf("There should be an error on decoding, %v", result)
+	}
+}
+
+func fakeServer(statusCode int, data string) *httptest.Server {
+	function := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(statusCode)
+		w.Header().Set("Content-Type", "text")
+		fmt.Fprint(w, data)
+	}
+
+	return httptest.NewServer(http.HandlerFunc(function))
+}
+
+func TestGET(t *testing.T) {
+	expected := "Some text"
+	server := fakeServer(http.StatusOK, expected)
+	defer server.Close()
+
+	data, err := GET(server.URL)
+	defer data.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, errReading := ioutil.ReadAll(data)
+	if errReading != nil {
+		t.Error(err)
+	}
+
+	resultString := string(result)
+
+	if expected != resultString {
+		t.Errorf("Wrong result, %v", resultString)
 	}
 }
